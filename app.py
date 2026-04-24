9f12a4ca0274d06da4b066fdb54f212d823971da
hypertec2010-sudo <hypertec2010@gmail.com>
2026-04-24 15:16:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6069,7 +6069,7 @@ with st.expander("Hilfen & Verwaltung", expanded=False):
     st.markdown("#### Technik / Admin")
     test1, test2 = st.columns([1.2, 2.8])
     with test1:
-        if st.button("Telegram-Test senden", use_container_width=True, key="telegram_test_button"):
+        if st.button("Telegram-Test senden", use_container_width=True, key=f"telegram_test_button_{admin_render_ns}"):
             test_message = (
                 f"Capital Hill Test\n"
                 f"Version: {APP_VERSION}\n"

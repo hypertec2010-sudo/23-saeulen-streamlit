5ff38cb5cd74394a84b8a863ffbe2fbaeb53831f
hypertec2010-sudo <hypertec2010@gmail.com>
2026-05-08 20:58:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 if not st.user.is_logged_in:
-    st.button("Mit Google anmelden", on_click=st.login, args=["google"])
+    st.button("Mit Google anmelden", on_click=st.login)
     st.stop()
 
-st.write(f"Willkommen, {st.user.name}")
+st.success("Login erfolgreich")
+st.write(st.user)
 st.button("Abmelden", on_click=st.logout)
